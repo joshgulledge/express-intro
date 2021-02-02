97ce9682b1f9f2b30ab6cde9aad0a0a5585e1025
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-02 15:31:04 -0600

    playing with server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const app = express();
 
 const port = 3000;
 
+// INCANTATION:
+// share any files inside the "public" folder
+
+app.use(express.static('server/public'));
+
 // listen for network request
 app.listen(port, function () {
   // when server is ready call this function
