cab4d320b7b0bbc33fc84641badb9db0f8e56f4e
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-03 16:39:29 -0600

    add quotes

--- server/public/scripts.js/client.js
+++ server/public/scripts.js/client.js
@@ -5,10 +5,44 @@ $(document).ready(onReady);
 function onReady() {
   console.log('its linked');
 
+  fetchQuotes();
+
+  $(document).on('submit', '#newQuoteForm', onSubmit);
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log('howdy');
+
+  let newQuote = {
+    quote: $('#quoteInput').val(),
+    author: $('#authorInput').val(),
+  };
+
+  // post
+
+  $.ajax({
+    url: '/quotes',
+    data: {
+      quote_to_add: newQuote,
+    },
+    method: 'POST',
+  })
+    .then(function (response) {
+      console.log('Oh yea');
+    })
+    .catch(function (error) {
+      console.log('blaaahhh');
+    });
+}
+
+function fetchQuotes() {
   let ajaxOptions = {
     url: '/allTheQuotes',
     method: 'GET',
   };
+
+  // get
 
   $.ajax(ajaxOptions)
     .then(function (response) {
@@ -19,18 +53,15 @@ function onReady() {
       for (let quote of quoteList) {
         console.log(quote);
         $('ul').append(`
-        <li> 
-          ${quote.quote} ${quote.author}
-        </li>
-      `);
+      <li> 
+        ${quote.quote} ${quote.author}
+      </li>
+    `);
       }
     })
     .catch(function () {
       $('#err').append(`
-      <h3> ooohhhh noooo </h3>
-      `);
+    <h3> ooohhhh noooo </h3>
+    `);
     });
 }
-
-// grab data from server
-// get allthequotes
