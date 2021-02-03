7635c8c4f41ddf417c5bb77d115a6263bb9ae310
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-03 15:58:24 -0600

    .then and .catch

--- server/public/scripts.js/client.js
+++ server/public/scripts.js/client.js
@@ -10,11 +10,26 @@ function onReady() {
     method: 'GET',
   };
 
-  $.ajax(ajaxOptions).then(function (response) {
-    console.log('got a response', response);
-  });
+  $.ajax(ajaxOptions)
+    .then(function (response) {
+      console.log('got a response', response);
 
-  console.log('so much to do');
+      let quoteList = response;
+
+      for (let quote of quoteList) {
+        console.log(quote);
+        $('ul').append(`
+        <li> 
+          ${quote.quote} ${quote.author}
+        </li>
+      `);
+      }
+    })
+    .catch(function () {
+      $('#err').append(`
+      <h3> ooohhhh noooo </h3>
+      `);
+    });
 }
 
 // grab data from server
