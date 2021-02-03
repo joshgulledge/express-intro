752bfc99caf4a73778f64263c397a2f3f85bf8a3
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-03 10:09:06 -0600

    working on getting data and not files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 // console.log(express);
+const quotes = require('./modules/quotes');
 
 // create our app
 const app = express();
@@ -11,8 +12,20 @@ const port = 3000;
 
 app.use(express.static('server/public'));
 
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// ^all the above is the same
+
 // listen for network request
 app.listen(port, function () {
   // when server is ready call this function
   console.log('I am listening...', port);
+});
+
+app.get('/quotes', function (req, res) {
+  console.log('GET request for quotes', req);
+  res.send(quotes());
 });
